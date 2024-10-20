f595ed121ba8664438cbc0d2f784e09245222473
epatjas <elina.patjas@gmail.com>
2024-10-20 14:55:47 +0300

    fixed a scheme related warning in expo app

--- app.config.js
+++ app.config.js
@@ -1,6 +1,7 @@
 module.exports = function(config) {
   return {
     ...config,
+    scheme: "yomi",
     extra: {
       EXPO_PUBLIC_SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL,
       EXPO_PUBLIC_SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
