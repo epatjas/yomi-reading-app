2d743ec00ac84fc8da052d467f255ac4aa931dc1
epatjas <elina.patjas@gmail.com>
2024-12-18 12:58:01 +0200

    minor UI updates

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { ArrowLeft, MoreVertical } from 'lucide-react-native';
+import { ArrowLeft } from 'lucide-react-native';
 import { colors, fonts, layout } from '../../app/styles/globalStyles';
 
 interface HeaderProps {
   title: string;
   onBackPress: () => void;
-  onMorePress: () => void;
 }
 
-const Header: React.FC<HeaderProps> = ({ title, onBackPress, onMorePress }) => {
+const Header = ({ title, onBackPress }: HeaderProps) => {
   return (
     <View style={styles.header}>
       <TouchableOpacity onPress={onBackPress} style={styles.headerButton}>
         <ArrowLeft size={24} color={colors.text} />
       </TouchableOpacity>
       <Text style={styles.title}>{title}</Text>
-      <TouchableOpacity onPress={onMorePress} style={styles.headerButton}>
-        <MoreVertical size={24} color={colors.text} />
-      </TouchableOpacity>
+      <View style={styles.headerButton} />
     </View>
   );
 };
@@ -28,12 +25,14 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    paddingHorizontal: layout.padding,
-    paddingTop: layout.padding,
-    paddingBottom: layout.padding / 2,
+    paddingHorizontal: 12,
+    height: 64,
   },
   headerButton: {
-    padding: 8,
+    width: 40,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   title: {
     fontFamily: fonts.medium,
